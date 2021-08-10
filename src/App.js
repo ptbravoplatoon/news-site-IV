2adea4f19b3e56e86b9a743bcb9e0649f5af493d
rbnsngrgg <54326272+rbnsngrgg@users.noreply.github.com>
2021-08-09 21:27:59 -0400

    Gregg R class-based w/challenges

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
 import React, { Component } from 'react';
-import navItems from './data/navItems.json';
 import './App.css';
 import AppNav from './components/AppNav/AppNav.js';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ArticlePage from './pages/ArticlePage';
 import SectionPage from './pages/SectionPage';
-import ArticlesAPI from './api/ArticlesAPI.js';
 
 class App extends Component {
   state = {
-    articles: null,
-    searchText: null,
+    searchText: ""
   }
 
   async handleSearch(text){
-    try{
-      var articlesJson;
-      if(text.length != 0){
-        articlesJson = await ArticlesAPI.searchArticles(text);
-      }
-      else{
-        articlesJson = await ArticlesAPI.fetchArticles();
-      }
-      this.setState({
-        articles: articlesJson,
-        searchText: text
-      })
-    }
-    catch(error){
-      console.error('HomePage.handleSearch: error fetching data.', error)
-    }
+    this.setState({
+      searchText: text
+    })
   }
-  componentDidMount(){
-    this.handleSearch("");
-  }
+
   render() {
     return (
       <div>
@@ -43,9 +25,7 @@ class App extends Component {
         <Router>
           <AppNav handleSearch={(text) => this.handleSearch(text)}/>
           <div>
-            {//<Route exact path="/" component={HomePage}/>
-            }
-            <Route exact path="/" render={() => <HomePage articles={this.state.articles}/>}/>
+            <Route exact path="/" render={() => <HomePage searchText={this.state.searchText}/>}/>
             <Route exact path="/articles/:articleID" component={ArticlePage}/>
             <Route exact path="/sections/:sectionID" render={(props) => <SectionPage {...props} searchText={this.state.searchText}/>}/>
           </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,10 +3,39 @@ import ArticleList from '../components/ArticleList/ArticleList.js'
 import ArticlesAPI from '../api/ArticlesAPI.js';
 
 class HomePage extends Component {
+  state = {
+    articles: null
+  }
+  async componentDidMount(){
+    this.handleSearch(this.props.searchText);
+  }
+  async componentDidUpdate(prevProps){
+    if(this.props.searchText != prevProps.searchText)
+    {
+      this.handleSearch(this.props.searchText);
+    }
+  }
+  async handleSearch(text){
+    try{
+      var articlesJson;
+      if(text.length != 0){
+        articlesJson = await ArticlesAPI.searchArticles(text);
+      }
+      else{
+        articlesJson = await ArticlesAPI.fetchArticles();
+      }
+      this.setState({
+        articles: articlesJson,
+      })
+    }
+    catch(error){
+      console.error('HomePage.handleSearch: error fetching data.', error)
+    }
+  }
   render() {
     return (
       <div>
-        <ArticleList articles={this.props.articles}/>
+        <ArticleList articles={this.state.articles}/>
       </div>
     );
   }
